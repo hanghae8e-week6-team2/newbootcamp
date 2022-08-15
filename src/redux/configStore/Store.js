14f408afa2918d8f948157e22c07dba3ea2afe69
mac <mac@macui-MacBookAir.local>
2022-08-15 15:18:59 +0900

    store.js 충돌 해결

--- src/redux/configStore/Store.js
+++ src/redux/configStore/Store.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { joinSlice } from "../modules/JoinSlice";
-<<<<<<< HEAD
+import { loginSlice } from "../modules/LoginSlice";
 import { detailSlice } from "../modules/DetailSlice";
 import { replySlice } from "../modules/ReplySlice";
 
@@ -8,20 +8,14 @@ import { replySlice } from "../modules/ReplySlice";
 const reducer = combineReducers({
   joinSlice: joinSlice.reducer,
   detailSlice: detailSlice.reducer,
-  replySlice: replySlice.reducer
+  replySlice: replySlice.reducer,
+  loginSlice: loginSlice.reducer,
 })
 
 export default configureStore({
   reducer,
    devTools: process.env.NODE_ENV !== "development",
-=======
-import { loginSlice } from "../modules/LoginSlice";
+})
 
-export default configureStore({
-  reducer: {
-    joinSlice: joinSlice.reducer,
-    loginSlice: loginSlice.reducer,
-  },
-  devTools: process.env.NODE_ENV !== "development",
->>>>>>> origin
-});
+
+
