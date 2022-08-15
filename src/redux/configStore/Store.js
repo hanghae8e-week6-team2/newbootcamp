347b0a87bbf9e95f2734aa60c0e7b79e4b6cc57a
whynotwhywhywhywhy <naomi981104@gmail.com>
2022-08-15 10:49:34 +0900

    Mainpage update

--- src/redux/configStore/Store.js
+++ src/redux/configStore/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { joinSlice } from "../modules/JoinSlice";
 import { MainSlice } from "../modules/MainSlice"
 
 export default configureStore({
     reducer: {
+        joinSlice: joinSlice.reducer,
         MainSlice: MainSlice.reducer,
     },
     devTools: process.env.NODE_ENV !== "development",
